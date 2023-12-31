d30111443124abd8dcc58a8dd3f12db0b7d82de0
Carlos Andres Alzate <unarteman@gmail.com>
2023-12-30 21:09:11 -0300

    agrego  al evento el poder quitar y agregar la clase bg-transparent

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,51 @@
 console.log("soy el script.js");
+const navbar = document.querySelector('.navbar-collapse');
+const searchIcon = document.getElementById('icon');
+const searchInput = document.getElementById('search');
+
+navbar.addEventListener('click', (e) => {
+    if (e.target.id === 'icon') {
+      toggleSearch();
+    }
+  });
+  
+  searchInput.addEventListener('blur', () => {
+    setTimeout(() => {
+      if (!searchInput.contains(document.activeElement)) {
+        hideSearch();
+      }
+    }, 0);
+  })
+  
+  function toggleSearch() {
+    searchIcon.classList.toggle('active');
+    searchInput.classList.toggle('d-none');
+    searchInput.classList.toggle('active');
+
+    const searchIconContainer = searchIcon.parentNode;
+
+    searchIconContainer.classList.toggle('bg-transparent');
+    
+    if (searchIcon.classList.contains('active')) {
+      searchInput.focus();
+    } else {
+      hideSearch();
+    }
+  }
+  
+  function hideSearch() {
+    const searchIconContainer = searchIcon.parentNode;
+    searchInput.classList.add('d-none');
+    searchInput.value = '';
+    searchInput.removeEventListener('blur', onBlur);
+    searchIconContainer.classList.add('bg-transparent');
+  }
+  
+  searchInput.addEventListener('blur', onBlur);
+  
+  function onBlur() {
+    if (searchInput.classList.contains('active')) {
+      hideSearch();
+    }
+  }
+  
