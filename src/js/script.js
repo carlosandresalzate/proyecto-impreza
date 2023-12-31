43204216e518c3de4c1a223d401535d7d7919998
Carlos Andres Alzate <unarteman@gmail.com>
2023-12-31 15:38:34 -0300

    remouevo y actualizo funciones. agrego la funcion setupSearchHandler, setupBackground, SearchHandler y backgroundImage. con esto se amaneja la funcionalidad de busqueda de la barra de navegacion y agregar background a diferentes partes de la pagina

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,20 @@
-console.log("soy el script.js");
-const navbar = document.querySelector('.navbar-collapse');
-const searchIcon = document.getElementById('icon');
-const searchInput = document.getElementById('search');
+// Función para la barra de búsqueda
+function setupSearchHandler() {
+  const searchHandler = new SearchHandler();
+}
 
-navbar.addEventListener('click', (e) => {
-    if (e.target.id === 'icon') {
-      toggleSearch();
-    }
-  });
-  
-  searchInput.addEventListener('blur', () => {
-    setTimeout(() => {
-      if (!searchInput.contains(document.activeElement)) {
-        hideSearch();
-      }
-    }, 0);
-  })
-  
+// Función para la imagen de fondo
+function setupBackgroundImage() {
+  const heroImg = document.getElementById('hero-img');
+  backgroundImage(heroImg);
+}
+
+// Objeto que maneja la barra de búsqueda
+function SearchHandler() {
+  const navbar = document.querySelector('.navbar-collapse');
+  const searchIcon = document.getElementById('icon');
+  const searchInput = document.getElementById('search');
+
   function toggleSearch() {
     searchIcon.classList.toggle('active');
     searchInput.classList.toggle('d-none');
@@ -25,14 +23,14 @@ navbar.addEventListener('click', (e) => {
     const searchIconContainer = searchIcon.parentNode;
 
     searchIconContainer.classList.toggle('bg-transparent');
-    
+
     if (searchIcon.classList.contains('active')) {
       searchInput.focus();
     } else {
       hideSearch();
     }
   }
-  
+
   function hideSearch() {
     const searchIconContainer = searchIcon.parentNode;
     searchInput.classList.add('d-none');
@@ -40,12 +38,53 @@ navbar.addEventListener('click', (e) => {
     searchInput.removeEventListener('blur', onBlur);
     searchIconContainer.classList.add('bg-transparent');
   }
-  
-  searchInput.addEventListener('blur', onBlur);
-  
+
   function onBlur() {
     if (searchInput.classList.contains('active')) {
       hideSearch();
     }
   }
-  
+
+  // Event listeners
+  navbar.addEventListener('click', (e) => {
+    if (e.target.id === 'icon') {
+      toggleSearch();
+    }
+  });
+
+  searchInput.addEventListener('blur', () => {
+    setTimeout(() => {
+      if (!searchInput.contains(document.activeElement)) {
+        hideSearch();
+      }
+    }, 0);
+  });
+
+  searchInput.addEventListener('blur', onBlur);
+}
+
+// Función para establecer la imagen de fondo
+function backgroundImage() {
+  const element = arguments[0];
+  const image = arguments[1] || "https://via.placeholder.com/1200x600";
+  const size = arguments[2] || "cover";
+  const position = arguments[3] || "center";
+
+  const background = {
+    image: image,
+    size: size,
+    position: position,
+
+    setBackground() {
+      element.style.backgroundImage = `url(${this.image})`;
+      element.style.backgroundSize = `${this.size}`;
+      element.style.backgroundPosition = `${this.position}`;
+    }
+  };
+
+  background.setBackground();
+}
+
+// Llamadas a las funciones
+setupSearchHandler();
+setupBackgroundImage();
